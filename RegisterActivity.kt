9d53be7cc0f02dedad1deb643ebf0cb6412cfcc7
azdindah098-web <azdindah098@gmail.com>
2026-02-06 15:14:26 +0100

    Create RegisterActivity.kt file

---
+++ RegisterActivity.kt
@@ -0,0 +1,13 @@
+// RegisterActivity.kt
+
+package com.example.instagramclone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
